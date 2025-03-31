20def35e87f12aefaafee93b73aaab26a08baa52
Shriyansh Agarwal <shriyanshagar9026@gmail.com>
2025-03-31 17:24:42 +0530

    Adding the changes the project.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,26 @@
 import express from 'express';
+import { graphqlHTTP as expressgraphql } from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 const app = express();
+
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name:'HelloWorld',
+        fields:{
+            message:{type:GraphQLString,
+                resolve(){
+                    return "Hello World"
+                }
+            },
+        }
+    }
+    )
+}
+)
+
+app.use('/graphql', expressgraphql({
+    schema:schema,
+  graphiql:true
+}))
 app.listen(5000, () => console.log('Server started'));
